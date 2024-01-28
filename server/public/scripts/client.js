8184f5fc2e6cca7408be91a008acf7291032f7fa
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-01-27 21:47:26 -0600

    created the base for the function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
 console.log('client.js is sourced!');
+
+let operation = "+"
+
+function submitCalculation() {
+    console.log("hello")
+}
+
+function captureOperation(operationSign) {
+    operation = operationSign
+}
+
+function fetchHistory() {
+    console.log("hello")
+
+}
+
+function renderHistory() {
+    console.log("hello")
+
+}
+function clearInput() {
+    console.log("hello")
+
+
+}
